967ad11ecb8debda3d7456f1b63004563b271379
Avina Kalle <59176676+Avina-cK@users.noreply.github.com>
2023-06-23 14:53:23 +0200

    Update CreatingMatrixAndNetworkGraph.r

--- ControlMachineLearning/FinalProject_ControlledCA/TransitionGraph/CreatingMatrixAndNetworkGraph.r
+++ ControlMachineLearning/FinalProject_ControlledCA/TransitionGraph/CreatingMatrixAndNetworkGraph.r
@@ -1,5 +1,3 @@
-# sets working directory
-setwd(".../TransitionGraph")
 
 source("ECA_with_control.R");
 # imports a function called ECA_controlled(n, N, T, Rand, initial_state, control)
@@ -10,6 +8,9 @@ source("ECA_with_control.R");
 # initial_state = a specific initial condition or NULL
 # control = T×2 matrix with the border controls
 
+rule_no<-105;  # rule number 
+N <- 3;        # no. of cells
+
 # Creating a transition matrix/ graph
 # Step 0: Possible controls
 pos_controls <- expand.grid(rep(list(0:1), 2))
@@ -17,44 +18,39 @@ as.matrix(pos_controls) -> pos_controls
 colnames(pos_controls)<-NULL
 
 # Step 1: Create all possible combinations of configuration for given N
-N <- 3;
 pos_comb <-  expand.grid(rep(list(0:1), N))
 as.matrix(pos_comb) -> pos_comb_matrix 
 colnames(pos_comb_matrix)<-NULL
 
-# image(1:N, 1:(2^N), rot(pos_comb_matrix)
-#     , main="Possible configurations"
-#     , axes = TRUE, xlab = "configuration")
-
 # Step 2: initiating Transition matrix
-rule_no<-90;
+TransitionMatrix<-matrix(0, ncol=(2^N), nrow=(2^N)) #initiating Transition matrix
 
-#initiating Transition matrix
-TransitionMatrix<-matrix(0, ncol=(2^N), nrow=(2^N))
 T = 2;
+# Step 3: Filling values of Transition Matrix
 for (j in 1:nrow(TransitionMatrix)){
-  s0<-pos_comb_matrix[j,];  
+  s0<-pos_comb_matrix[j,];  # set the initial configuration from the possible combinations
   for (i in 1:4) {
-    u1<-pos_controls[i,]
-    u2<-c(0,0)
+    u1<-pos_controls[i,] # set control from its possible combinations
+    u2<-c(0,0)  # final "control" is just (0,0)
     u <- matrix(c(u1, u2), nrow=2, byrow=TRUE);
-    ECA_trial<-ECA_controlled(rule_no, N, T, s0, u);
-    ECA_final<-ECA_trial[T,3:ncol(ECA_trial)-1]
+    ECA_trial<-ECA_controlled(rule_no, N, T, s0, u);  
+    ECA_final<-ECA_trial[T,3:ncol(ECA_trial)-1] #Final state with s_0 and u1
     matching_row <- which(apply(pos_comb_matrix, 1, function(row) all(row == ECA_final)))
     TransitionMatrix[j,matching_row]+1->TransitionMatrix[j,matching_row]
-    print(matching_row)
   }
 }
 
 library(igraph)
-
+# creates a graph from the Transition (Adjacency) matrix
 graph_from_adjacency_matrix(TransitionMatrix)->plotgraph
-l <-  layout_with_gem(plotgraph)
-plot(  plotgraph
+l <-  layout_with_kk(plotgraph)
+# plots the transition graph                                
+plot(  plotgraph  
      , edge.arrow.size=.4
      , edge.color = "purple"
      , vertex.color = "white"
      , layout = l
      , edge.curved=0
+     , main=paste("Rule", rule_no)
      )
-# reference for plotting : https://kateto.net/network-visualization
+# reference: https://kateto.net/network-visualization
